3927221938f73c9b143cf1fae9925415b1a7bdd7
kristisaare <kristisaare@gmail.com>
2016-05-21 13:58:46 +0300

    array

--- calculations_test.php
+++ calculations_test.php
@@ -14,8 +14,16 @@ echo "Soovid investeerida $lengthMonth kuuks, intressiga $interestMonthly % kuus
 $initialInvestmentGrowth = $initialInvestment*pow((1+$interestMonthly/100), $lengthMonth);
 $monthlyInvestmentGrowth = $monthlyInvestment*((pow((1+$interestMonthly/100), $lengthMonth)-1)/($interestMonthly/100));
 echo "Alginv kasv $initialInvestmentGrowth ja lisatud raha kasv $monthlyInvestmentGrowth";
-$finalInvestment = ($initialInvestmentGrowth+$monthlyInvestmentGrowth);
+$finalInvestment = round(($initialInvestmentGrowth+$monthlyInvestmentGrowth), 2);
 echo "Lõplik investeeringu suurus pärast $lengthYear aastat on $finalInvestment";
+
+$array[] = $lengthMonth;
+for ($i=0; $i < $lengthMonth; $i++) {
+  $currentInvestment = ($initialInvestment*pow((1+$interestMonthly/100), $i))+$monthlyInvestment*((pow((1+$interestMonthly/100), $i)-1)/($interestMonthly/100));
+  $array[$i] = $currentInvestment;
+}
+
+print_r($array);
 
 
  ?>
