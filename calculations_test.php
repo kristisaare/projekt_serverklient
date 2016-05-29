9aeda782d0b2f2ef3b9926b9cba30a63d2432688
kristisaare <kristisaare@gmail.com>
2016-05-29 21:41:41 +0300

    chartbase, chartvisual

--- calculations_test.php
+++ calculations_test.php
@@ -17,24 +17,31 @@ $monthlyInvestmentGrowth = $monthlyInvestment*((pow((1+$interestMonthly/100), $l
 $finalInvestment = round(($initialInvestmentGrowth+$monthlyInvestmentGrowth), 2);
 // echo "Lõplik investeeringu suurus pärast $lengthYear aastat on $finalInvestment";
 
-$arrayTotalInvestments[] = $lengthMonth;
+$arrayTotalInvestments = array();
 for ($i=0; $i < $lengthMonth; $i++) {
   $currentInvestment = round(($initialInvestment*pow((1+$interestMonthly/100), $i))+$monthlyInvestment*((pow((1+$interestMonthly/100), $i)-1)/($interestMonthly/100)), 2);
   $arrayTotalInvestments[$i] = $currentInvestment;
 }
 
-$arrayTotalPrincipal[] = $lengthMonth;
+$arrayTotalPrincipal = array();
 for ($j=0; $j < $lengthMonth ; $j++) {
   $currentPrincipal = ($initialInvestment+($monthlyInvestment*$j));
   $arrayTotalPrincipal[$j] = $currentPrincipal;
 }
 
+$arrayTotalInterest = array();
+for ($k=0; $k < $lengthMonth ; $k++) {
+  $currentInterest = ($arrayTotalInvestments[$k]-$arrayTotalPrincipal[$k]);
+  $arrayTotalInterest[$k] = $currentInterest;
+}
+
 $result = array(
   "totalInvestments" => $arrayTotalInvestments,
   "totalPrincipal" => $arrayTotalPrincipal,
+  "totalInterest" => $arrayTotalInterest,
+  "lengthYear" => range(1, $lengthYear),
 );
 
-print_r($result);
-
+echo json_encode($result);
 
  ?>
--- js/app.js
+++ js/app.js
@@ -5,7 +5,59 @@ $(document).ready(function(){
     event.preventDefault();
     $.post("calculations_test.php", form.serialize())
     .done(function(data){
-      console.log(data);
+      var parsedData = $.parseJSON(data);
+      console.log(parsedData.totalInvestments);
+      drawChart(parsedData);
     });
-});
+  });
+  function drawChart(parsedData){
+    $('#container').highcharts({
+        chart: {
+            type: 'area'
+        },
+        title: {
+            text: 'Investeeritud raha kasv tulevikus'
+        },
+        xAxis: {
+            tickmarkPlacement: 'on',
+            ceiling: parsedData.lengthYear,
+            title: {
+                text: 'Investeeritud aastad'
+            },
+            // labels: {
+              // formatter: function(){
+                // return Math.round(this.value/12;
+              // }
+            // }
+        },
+        yAxis: {
+            title: {
+                text: 'Kogusumma eurodes'
+            },
+        },
+        tooltip: {
+            shared: true,
+        },
+        plotOptions: {
+            area: {
+                stacking: 'normal',
+                lineColor: '#666666',
+                lineWidth: 1,
+                marker: {
+                    lineWidth: 1,
+                    lineColor: '#666666'
+                }
+            }
+        },
+        series: [{
+          name: 'Inveesteeringu intressikasv',
+          data: parsedData.totalInterest
+        }, {
+            name: 'Ise investeeritud raha',
+            data: parsedData.totalPrincipal
+        }]
+    });
+  };
+
+
 });
