8043a5106c734ec5966780aef3ec106e0c81012d
kristisaare <kristisaare@gmail.com>
2016-06-01 20:12:16 +0300

    db

---
+++ db.php
@@ -0,0 +1,30 @@
+<?php
+  function connect_db(){
+  global $connection;
+  // Test server
+  // $host="localhost";
+  // $user="test";
+  // $pass="t3st3r123";
+  // $db="test";
+  // $port="3306";
+
+  $host="localhost";
+  $user="root";
+  $pass="root";
+  $db="test";
+  $port="8889";
+  $connection = mysqli_connect($host, $user, $pass, $db, $port) or die("ei saa ühendust mootoriga- ".mysqli_error());
+  mysqli_query($connection, "SET CHARACTER SET UTF8") or die("Ei saanud baasi utf-8-sse - ".mysqli_error($connection));
+}
+
+  connect_db();
+
+$query ="SELECT * FROM ksaare_historicalreturns WHERE 1";
+$result = mysqli_query($connection, $query) or die("$query - ".mysqli_error($connection));
+
+$results = array();
+while ($row = mysqli_fetch_assoc($result)) {
+  $results[] = $row;
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+require_once("db.php");
+?>
+<!DOCTYPE html>
+<head>
+      <meta charset="utf-8"/>
+      <title> Arvuta portfelli tulevikuväärtus </title>
+      <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+      <script src="https://code.highcharts.com/highcharts.js"></script>
+      <script src="js/app.js"></script>
+      <link rel="stylesheet" type="text/css" href="view.css">
+</head>
+<body>
+  <div id="frontview">
+      <div id="questions">
+        <form action="calculations_test.php" method="post" id="calculations-form">
+
+          <label>Praegu investeeritud summa</label>
+          <input type="text" placeholder="Portfelli väärtus" name="initialInvestment">
+
+          <label>Igakuiselt lisatav rahasumma</label>
+          <input type="text" placeholder="Igakuiselt lisatav rahasumma" name="monthlyInvestment">
+
+          <label>Mitmeks aastaks soovid raha investeerida?<label>
+          <input type="text" placeholder="Investeeringu pikkus aastates" name="lengthYear">
+
+          <label>Aastane keskmine tootlus protsentides</label>
+          <input type="text" placeholder="Tootlus %" name="interestAnnual">
+
+          <button type="submit">Arvuta!</button>
+        </form>
+        <div class="panel">
+              <div>Ajalooline tootlus:</div>
+
+              <?php
+                foreach($results as $res):
+                  $updated_at = date('d.m.Y', strtotime($res['updated_at']));
+              ?>
+                <div><?php echo "{$res['portal']} / {$res['historical_interest']}% / {$updated_at}" ?></div>
+              <?php endforeach; ?>
+        </div>
+      </div>
+
+      <div id="container"></div>
+  </div>
+<div id="table-container"></div>
+
+
+
+</body>
+</html>
